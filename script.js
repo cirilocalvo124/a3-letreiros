03db787ba24512f68b059a123b45bae2d44ecdd8
cirilocalvo124 <166167137+cirilocalvo124@users.noreply.github.com>
2024-04-05 16:13:26 -0300

    a3

---
+++ script.js
@@ -0,0 +1,9 @@
+function ver_instagram() {
+  window.location.href = 'https://www.instagram.com/candido.alex/?hl=pt_BR'
+
+}
+
+function mudar() {
+  window.location.href = 'https://github.com/cirilocalvo124/A3-letreiros-'
+}
+
